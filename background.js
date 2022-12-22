b5c515b6ab085a217f3e3f36ef43ddfff8f93f79
whitehathacker24 <119995201+whitehathacker24@users.noreply.github.com>
2022-12-22 14:50:42 -0800

    Create background.js

---
+++ background.js
@@ -0,0 +1,37 @@
+function moveBall() {
+    ball.move(e, r);
+    ball2.move(x, y);
+    ball3.move(e, y);
+    ball4.move(x, r);
+}
+function paintBall() {
+    ball.setColor(Randomizer.nextColor());
+    ball2.setColor(Randomizer.nextColor());
+    ball3.setColor(Randomizer.nextColor());
+    ball4.setColor(Randomizer.nextColor());
+}
+for (var i = 0; i < 10; i++) {
+var size = 40+10;
+}
+var x = -1;
+var y = -1;
+var e = 1;
+var r = 1;
+var t = 40;
+setTimer(moveBall, 50);
+var ball = new Circle(size);
+ball.setPosition(getWidth()/2, getHeight()/2);
+add(ball);
+setTimer(paintBall, 400);
+//ball 2 :D//
+var ball2 = new Circle(size);
+ball2.setPosition(getWidth()/2, getHeight()/2);
+add(ball2);
+//ball3 :D//
+var ball3 = new Circle(size);
+ball3.setPosition(getWidth()/2, getHeight()/2);
+add(ball3);
+//ball 4 :D//
+var ball4 = new Circle(size);
+ball4.setPosition(getWidth()/2, getHeight()/2);
+add(ball4);
