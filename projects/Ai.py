3f3f7205eed31ef8877799898134afffbedf1703
whitehathacker24 <119995201+whitehathacker24@users.noreply.github.com>
2023-03-13 12:31:40 -0700

    Create Ai.py

---
+++ projects/Ai.py
@@ -0,0 +1,19 @@
+<py-script>
+@app.route('/process_essay', methods=['POST'])
+def process_essay():
+    data = request.get_json()
+    title = data['title']
+    body = data['body']
+    
+    # Perform some processing on the essay
+    # For example, you could use a natural language processing library like NLTK
+    # to identify key themes and summarize the essay
+    result = f"Title: {title}\n\n{body}"
+    
+    return result
+
+if __name__ == '__main__':
+    app.run()
+{% endraw %}
+//run the code in flask
+  </py-script>
