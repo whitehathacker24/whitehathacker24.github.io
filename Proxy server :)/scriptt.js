dfdd43dd4ef92310bb9869fd10c657d1410ffadc
whitehathacker24 <119995201+whitehathacker24@users.noreply.github.com>
2023-05-14 17:49:41 -0800

    Update scriptt.js

--- Proxy server :)/scriptt.js
+++ Proxy server :)/scriptt.js
@@ -1,7 +1,7 @@
 function sendRequest() {
   // Send a request to the proxy server
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://localhost:8080?url=http://example.com"); // Change URL and port as necessary
+  xhr.open("GET", "https://google.com"); // Change URL and port as necessary
   xhr.onload = function() {
     console.log(xhr.responseText);
   };
