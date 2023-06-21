53bd2248d4b48fac91b6a191a9b1a7a6cf9b5695
whitehathacker24 <119995201+whitehathacker24@users.noreply.github.com>
2023-06-21 08:35:30 -0800

    eee

--- Proxy server :)/scriptt.js
+++
@@ -1,9 +0,0 @@
-function sendRequest() {
-  // Send a request to the proxy server
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://google.com"); // Change URL and port as necessary
-  xhr.onload = function() {
-    console.log(xhr.responseText);
-  };
-  xhr.send();
-}
